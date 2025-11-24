85ac388914f7f4c4cf95f830da95144e8c8e4118
sandhyarani <dokesandhya129@gmail.com>
2025-11-24 11:05:56 +0530

    inite

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,9 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
+
+
+        System.out.println("i am sandhya");
+        System.out.println("bjkbefkbhfiwbwibf");
     }
 }
